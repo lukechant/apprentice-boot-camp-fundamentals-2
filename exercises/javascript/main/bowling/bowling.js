b06246b31df66d7a2aec0e61872357e16ab1b5a9
lukechant <luke_chant@yahoo.co.uk>
2021-04-19 14:32:17 +0100

    add further refactor

--- exercises/javascript/main/bowling/bowling.js
+++ exercises/javascript/main/bowling/bowling.js
@@ -2,11 +2,9 @@ function bowl(rolls) {
     let score = 0;
     for(let i = 0; i < rolls.length; i += 2){
         if(rolls[i] + rolls[i + 1] === 10){
-            score += 10;
-            score += rolls[i + 2];
-;        } else {
-        score += rolls[i];
-        score += rolls[i + 1];
+            score += 10 + rolls[i + 2];
+        } else {
+        score += rolls[i] + rolls[i + 1];
     }
     }
     return score;
