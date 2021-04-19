3bb29cca7774200dd5ac62eca10598a306451ce0
lukechant <luke_chant@yahoo.co.uk>
2021-04-19 14:26:35 +0100

    add thrd working test

--- exercises/javascript/main/bowling/bowling.js
+++ exercises/javascript/main/bowling/bowling.js
@@ -1,8 +1,13 @@
 function bowl(rolls) {
     let score = 0;
     for(let i = 0; i < rolls.length; i += 2){
+        if(rolls[i] + rolls[i + 1] === 10){
+            score += 10;
+            score += rolls[i + 2];
+;        } else {
         score += rolls[i];
         score += rolls[i + 1];
+    }
     }
     return score;
 }
